bc4c573d4b6f4a34f21f33fe4b684a6d537712fd
Sam Kaiser <samuel.k.public@gmail.com>
2025-08-23 15:46:47 -0500

    Added an initial app loop and foundation for calling chosen options

--- Program.cs
+++ Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        bool appRunning = true;
+
+        String[] menuOptions = [
+            "Display Characters",
+            "Add Character",
+            "Level Up Character",
+            "Quit (or q)" //always last
+        ];
+
+        do
+        {
+            int i = 0;
+
+            foreach (String option in menuOptions)
+            {
+                i++;
+                Console.WriteLine($"{i}. {option}");
+            }
+
+            var response = Console.ReadLine();
+
+            Console.WriteLine($"You chose: {response}");
+
+            switch (response)
+            {
+                case "1": displayCharacters(); break;
+                case "2": addCharacter(); break; //TODO: change from static void to static Character method
+                case "3": levelCharacter(); break;
+                case "4": appRunning = false; break;
+                case "q": appRunning = false; break;
+                default: break;
+            }
+        } while (appRunning);
+    }
+
+    static void displayCharacters() {
+    }
+
+    static void addCharacter()
+    {
+    }
+
+    static void levelCharacter()
+    {
     }
 }
