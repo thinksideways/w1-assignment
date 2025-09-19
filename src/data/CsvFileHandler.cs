de46f15486ca418dc42290fe165f72fcb486c885
Sam Kaiser <samuel.k.public@gmail.com>
2025-09-18 19:40:49 -0500

    Return List of Character objects from CsvFileHandler class as per assignment guidelines

--- src/data/CsvFileHandler.cs
+++ src/data/CsvFileHandler.cs
@@ -1,7 +1,19 @@
+using System.Text.RegularExpressions;
+
 public class CsvFileHandler : IFileHandler
 {
-    public List<Character> ReadCharacters(string filePath) { 
-        return new List<Character>(); 
+    public List<Character> ReadCharacters(string filePath) {
+        List<String> characterCsvData = File.ReadAllLines(filePath).ToList();
+        characterCsvData.RemoveAt(0); // Remove header row before creating Character objects
+
+        List<Character> characters = new List<Character>();
+
+        foreach (string line in characterCsvData) {
+            var characterDetails = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            characters.Add(new Character(characterDetails[0], characterDetails[1], int.Parse(characterDetails[2]), int.Parse(characterDetails[3]), characterDetails[4].Split("|")));
+        }
+
+        return characters;
     }
     
     public void WriteCharacters(string filePath, List<Character> characters) { 
